fc39a294d34cf655f210169d1b08c20206d460f6
Clifton Benally <33295966+cbenally183@users.noreply.github.com>
2019-05-26 20:45:51 -0700

    screencap

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
-import keras
-from keras import layers
+#import tensorflow
+#import keras
+#from keras import layers
 
 #from keypress import PressKey, ReleaseKey, UP_ARROW, RIGHT_ARROW, DOWN_ARROW, LEFT_ARROW
 def screen_record():
     last_time = time.time()
     while(True):
-        printscreen = np.array(ImageGrab.grab(bbox=(0,40,1280,720)))
+        printscreen = np.array(ImageGrab.grab(bbox=(0,0,600,600)))
+        print(printscreen.shape)
         #print('loop took {} seconds'.format(time.time() - last_time))
         last_time = time.time()
         cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-        print(printscreen.size)
 
 screen_record()
 #time.sleep(4)
@@ -25,8 +26,8 @@ screen_record()
 #ReleaseKey(UP_ARROW)
 #time(1)
 
-model_1 = keras.Sequential()
-model_1.add(layers.Dense(64,activation = 'relu'))
-model_1.add(layers.Dense(64))
+#model_1 = keras.Sequential()
+#model_1.add(layers.Dense(64,activation = 'relu'))
+#model_1.add(layers.Dense(64))
 
 
