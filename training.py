e830bfe64b47c7844bbc9d22647edf116b9df28d
Fatkitties <33295966+Fatkitties@users.noreply.github.com>
2019-06-06 21:47:42 -0700

    update

--- training.py
+++ training.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import numpy as np
 import os
+import cv2
+from PIL import Image
+#import keras
 
 #Save Models Keras
 #https://machinelearningmastery.com/save-load-keras-deep-learning-models/
 
 
 np.random.seed(7)
+
+#import dataset
+image = Image.open("Training Data/red1.png")
+cv2.imshow('window', image)
+#model = keras.Sequential()
+#model.add(keras.layers.Dense(12))
+
+
+#save to Json
+#save_results = model.to_json()
