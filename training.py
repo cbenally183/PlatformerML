a65585596d8c376829edc8885d2567545d04b3ad
Fatkitties <33295966+Fatkitties@users.noreply.github.com>
2019-06-12 22:50:36 -0700

    update

--- training.py
+++ training.py
@@ -14,11 +14,11 @@ np.random.seed(7)
 #import dataset
 image = Image.open("Training Data/red1.png")
 cv2.imshow('window', image)
-#model = keras.Sequential()
-#model.add(keras.layers.Dense(12),activation="relu)
-#model.add(keras.layers.Dense(24),activation="relu)
-#model.add(keras.layers.Dense(20),activation="relu")
+model = keras.Sequential()
+model.add(keras.layers.Dense(12),activation="relu)
+model.add(keras.layers.Dense(24),activation="relu)
+model.add(keras.layers.Dense(20),activation="relu")
 
 
 #save to Json
-#save_results = model.to_json()
+save_results = model.to_json()
