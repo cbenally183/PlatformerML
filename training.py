427b17122ccff9982d906944f294911230843cad
Fatkitties <33295966+Fatkitties@users.noreply.github.com>
2019-06-27 22:38:03 -0700

    new

--- training.py
+++ training.py
@@ -4,21 +4,25 @@ import os
 import cv2
 from PIL import Image
 #import keras
-
+import random
 #Save Models Keras
 #https://machinelearningmastery.com/save-load-keras-deep-learning-models/
 
 
-np.random.seed(7)
+#np.random.seed(7)
 
 #import dataset
-image = Image.open("Training Data/red1.png")
-cv2.imshow('window', image)
-model = keras.Sequential()
-model.add(keras.layers.Dense(12),activation="relu)
-model.add(keras.layers.Dense(24),activation="relu)
-model.add(keras.layers.Dense(20),activation="relu")
-model.add(keras.layers.Dense(18),activation="relu")
+#image = Image.open("Training Data/red1.png")
+#cv2.imshow('window', image)
+#model = keras.Sequential()
+#model.add(keras.layers.Dense(12),activation="relu)
+#model.add(keras.layers.Dense(24),activation="relu)
+#model.add(keras.layers.Dense(20),activation="relu")
+#model.add(keras.layers.Dense(18),activation="relu")
 
 #save to Json
-save_results = model.to_json()
+#save_results = model.to_json()
+
+
+print(random.randint(0,51))
+print(random.randint(0,51))
