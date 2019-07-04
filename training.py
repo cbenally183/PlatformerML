7eb90377aecc837622970ee1610217fa5fcc5930
Fatkitties <33295966+Fatkitties@users.noreply.github.com>
2019-07-03 22:36:16 -0700

    dds

--- training.py
+++ training.py
@@ -3,7 +3,9 @@ import numpy as np
 import os
 import cv2
 from PIL import Image
+from scipy import misc
 #import keras
+import glob
 import random
 #Save Models Keras
 #https://machinelearningmastery.com/save-load-keras-deep-learning-models/
@@ -12,9 +14,12 @@ import random
 #np.random.seed(7)
 
 #import dataset
-image_numpy = Image.open("Training Data/red1.png")
-image = cv2.fromarray(image_numpy)
-cv2.imshow('window', image)
+path = 1
+print("Test")
+image = misc.imread("Training Data/red1.png")
+print(image)
+print(image.shape)
+print(image.dtype)
 #model = keras.Sequential()
 #model.add(keras.layers.Dense(12),activation="relu)
 #model.add(keras.layers.Dense(24),activation="relu)
